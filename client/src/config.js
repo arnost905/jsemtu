74aa966613efb9d298112a8095c413ae8e1e7541
Karel Půček <arnost905@gmail.com>
2026-07-18 08:48:09 +0200

    Verze 0.2.0 - vlastní modální výběr dne a hodiny - export přehledu do CSV - odstraněno odesílání e-mailem - úpravy UI SummaryPanel

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,9 @@
-const API_URL = `${window.location.protocol}//${window.location.host}/JsemTu/api`;
+const isLocal =
+  window.location.hostname === "localhost" ||
+  window.location.hostname.startsWith("172.");
+
+const API_URL = isLocal
+  ? `http://${window.location.hostname}/JsemTu/api`
+  : "https://jskod.cz/jsemtu/api";
 
 export default API_URL;
